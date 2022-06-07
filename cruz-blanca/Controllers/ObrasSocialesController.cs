cf916368214a70c561f56e74032f034e28da20cd
galobponce <personal.galo.bello@gmail.com>
2022-06-07 16:44:43 -0300

    added create for obras-sociales

--- cruz-blanca/Controllers/ObrasSocialesController.cs
+++ cruz-blanca/Controllers/ObrasSocialesController.cs
@@ -24,6 +24,31 @@ namespace cruz_blanca.Controllers
             return View(obrasSociales);
         }
 
+
+        // Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ObraSocial obraSocial = new ObraSocial();
+            return View(obraSocial);
+        }
+
+        [HttpPost]
+        public IActionResult Create(ObraSocial obraSocial)
+        {
+            int Id = _context.ObrasSociales.Max(item => item.Id);
+            Id += 1;
+
+            obraSocial.Id = Id;
+
+            this._context.Attach(obraSocial);
+            this._context.Entry(obraSocial).State = EntityState.Added;  
+            this._context.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+
+        // Edit
         [HttpGet]
         public IActionResult Edit(string Id)
         {
@@ -36,6 +61,24 @@ namespace cruz_blanca.Controllers
         {
             this._context.Attach(obraSocial);
             this._context.Entry(obraSocial).State = EntityState.Modified;
+            this._context.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+
+        // Delete
+        [HttpGet]
+        public IActionResult Delete(string Id)
+        {
+            ObraSocial obraSocial = this._context.ObrasSociales.Where(p => p.Id == int.Parse(Id)).FirstOrDefault();
+            return View(obraSocial);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(ObraSocial obraSocial)
+        {
+            this._context.Attach(obraSocial);
+            this._context.Entry(obraSocial).State = EntityState.Deleted;
             this._context.SaveChanges();
             return RedirectToAction("index");
         }
