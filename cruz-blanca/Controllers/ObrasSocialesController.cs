7cc91bd13c1a7e7758e263263108362fa2c0d9bd
galobponce <personal.galo.bello@gmail.com>
2022-06-07 16:01:26 -0300

    added obras-sociales crud template

--- cruz-blanca/Controllers/ObrasSocialesController.cs
+++ cruz-blanca/Controllers/ObrasSocialesController.cs
@@ -2,18 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using cruz_blanca.Data;
+using cruz_blanca.Models;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using Microsoft.EntityFrameworkCore;
 
 namespace cruz_blanca.Controllers
 {
     public class ObrasSocialesController : Controller
     {
-        // GET: /<controller>/
+        private readonly CruzBlancaDBContext _context; 
+
+        public ObrasSocialesController(CruzBlancaDBContext context)
+        {
+            this._context = context; 
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<ObraSocial> obrasSociales = this._context.ObrasSociales.ToList();
+            return View(obrasSociales);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string Id)
+        {
+            ObraSocial obraSocial = this._context.ObrasSociales.Where(p => p.Id == int.Parse(Id)).FirstOrDefault();
+            return View(obraSocial);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ObraSocial obraSocial)
+        {
+            this._context.Attach(obraSocial);
+            this._context.Entry(obraSocial).State = EntityState.Modified;
+            this._context.SaveChanges();
+            return RedirectToAction("index");
         }
     }
 }
